c79396cd4c8ad4fe35538cacd7ac348cd27e68b9
Jonathan Vieira <jonathansouza59@gmail.br>
2023-05-20 18:15:15 -0300

    chore: remove ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgxAutocompleteComponent } from './ngx-autocomplete/ngx-autocomplete.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NgxAutocompleteComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TooltipModule.forRoot(),
     BrowserAnimationsModule,
-    BsDropdownModule.forRoot(),
-    TabsModule.forRoot(),
-    TypeaheadModule.forRoot(),
     ReactiveFormsModule,
     FormsModule
-    
   ],
   providers: [],
   bootstrap: [AppComponent]
